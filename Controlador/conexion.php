d65aa09dea5ba60f4e108e0d9203779c79b0d51c
Fabricio David Galarza <fdgalarza@indracompany.com>
2022-12-06 21:46:02 -0500

    conexión

--- Controlador/conexion.php
+++ Controlador/conexion.php
@@ -3,10 +3,10 @@
 	function CrearConexion(){
 		
 		//conexion al servidor
-		$conexion = mysqli_connect("localhost", "root", "") OR DIE("No es posible establecer conexión con el servidor"); 
+		$conexion = mysql_connect("localhost", "root", "") OR DIE("No es posible establecer conexión con el servidor"); 
 		
 		//se selecciona la base de datos a la cual se realizara la conexion
-		mysqli_select_db($conexion, "ic_proyecto") OR DIE("No es posible establecer conexion con la base de datos");
+		mysql_select_db($conexion, "ic_proyecto") OR DIE("No es posible establecer conexion con la base de datos");
 		//mysqli_select_db("localhost","root","","ic_proyecto") OR DIE("No es posible establecer conexion con la base de datos");
 		return $conexion;
 	}
