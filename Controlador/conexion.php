bef8e5323ed42fca47f93b887c4780c856e47e6b
Fabricio David Galarza <fdgalarza@indracompany.com>
2022-12-08 10:44:38 -0500

    Commint para mostrar integración continua con jenkins y docker

--- Controlador/conexion.php
+++ Controlador/conexion.php
@@ -3,10 +3,10 @@
 	function CrearConexion(){
 		
 		//conexion al servidor
-		$conexion = mysql_connect("localhost", "root", "") OR DIE("No es posible establecer conexión con el servidor"); 
+		$conexion = mysqli_connect("localhost", "root", "") OR DIE("No es posible establecer conexión con el servidor"); 
 		
 		//se selecciona la base de datos a la cual se realizara la conexion
-		mysql_select_db($conexion, "ic_proyecto") OR DIE("No es posible establecer conexion con la base de datos");
+		mysqli_select_db($conexion, "ic_proyecto") OR DIE("No es posible establecer conexion con la base de datos");
 		//mysqli_select_db("localhost","root","","ic_proyecto") OR DIE("No es posible establecer conexion con la base de datos");
 		return $conexion;
 	}
